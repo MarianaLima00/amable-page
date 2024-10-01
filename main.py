17bf0708603b5341d9359bb468932cc24109dbcc
marianalimatheodoro081@gmail.com <mari>
2024-10-01 11:24:58 -0300

    enviando arquivo req

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")  # Adicionando a importação do render_template
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/carrinho')
+def carrinho():
+    return render_template('carrinho.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
