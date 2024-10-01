58bcabf7f507bc2dc2d91e599e23b7e533ac27fd
marianalimatheodoro081@gmail.com <mari>
2024-10-01 08:29:23 -0300

    enviando primeira publicação do site

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
- from flask import Flask, render_template
- app = Flask(__name__)
+from flask import Flask, render_template  # Importando a função render_template
 
- @app.route('/')
- def index():
-  return render_template("index.html")
+app = Flask(__name__)
 
- if __name__ == '__main__':
- app.run(debug=True)
+
+@app.route('/')
+def index():
+    return render_template("index.html")  # Adicionando a importação do render_template
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
